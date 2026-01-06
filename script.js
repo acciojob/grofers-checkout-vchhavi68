46af7ab93fa188a8e9083f9021690994a4a11876
vchhavi68 <vchhavi68@gmail.com>
2026-01-06 14:16:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const prices = document.querySelectorAll(".prices");
-  let total = 0;
+ function calculateTotal() {
+    const prices = document.querySelectorAll(".prices");
+    let total = 0;
 
-  prices.forEach(price => {
-    total += Number(price.innerText);
-  });
+    prices.forEach(price => {
+      total += Number(price.innerText);
+    });
 
-  const table = document.getElementById("groceryTable");
-  const row = document.createElement("tr");
-  const cell = document.createElement("td");
-
-  cell.colSpan = 2;
-  cell.innerText = "Total Price: " + total;
-
-  row.appendChild(cell);
-  table.appendChild(row);
+    document.getElementById("ans").innerText = total;
+  }
